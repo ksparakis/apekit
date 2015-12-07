da69e28e97d0eb92bafb796ecb19242cb219555c
ksparakis <ksparakis@carbonite.com>
2015-12-07 12:36:14 -0500

    Merged lukes debuge mode for output

--- vulns/keySearch.py
+++ vulns/keySearch.py
@@ -2,15 +2,17 @@
 # **Parameters : inputString- a line of code or a string 
 # **Output : False if key does not look like a private key of anykind
 #	     Returns Array[True, Keytype]- keytype will be null if cant specifiy 
+DEBUG = False
+
 def keySearch ( inputString ):
 	if ressemblesKey(inputString):
 		keyType = keyTypeDetection(inputString)
 		return [True, keyType];			
 
-	return False;
+	return [False, None];
 
 def keyTypeDetection(inputString):
-	return "null";
+	return None;
 
 def ressemblesKey(inputString):
 	extract = inQuotes(inputString) 
@@ -28,7 +30,7 @@ def ressemblesKey(inputString):
 		unique_upperCase = uniqueUpperCaseCount(extract)
 		unique_lowerCase = uniqueLowerCaseCount(extract)
 
-		if unique_letters > 5 or numbers == len(extract) or unique_upperCase == len(extract) or (unique_letters > 2 and symbols > 0 and unique_upperCase > 0 and unique_lowercase):
+		if unique_letters > 5 or numbers == len(extract) or unique_upperCase == len(extract) or (unique_letters > 2 and symbols > 0 and unique_upperCase > 0 and unique_lowerCase):
 			return True;
 	return False;
 
@@ -37,28 +39,35 @@ def inQuotes(inputString):
 	import re
 	result = re.findall(r'"([^"]*)"', inputString)
 	if result != "":
-		print "Extracted string = " + str(result)
+		if DEBUG:
+			print "Extracted string = " + str(result)
 		return result;
 	result = re.findall(r"'(.*?)'", inputString)
 	if result != "":
-		print "Extracted string = " + str(result)
+		if DEBUG:
+			print "Extracted string = " + str(result)
 		return result;
-	print "Was unable to extract potential string from - "+ str(inputString) 
+	if DEBUG:
+		print "Was unable to extract potential string from - "+ str(inputString) 
 	return False;
 
 def containsSpaces (inputString):
 	if ' ' in inputString:
-		print "\tContains spaces."
+		if DEBUG:
+			print "\tContains spaces."
 		return True;
-	print "\tDoes not contain spaces."
+	if DEBUG:
+		print "\tDoes not contain spaces."
 	return False;
 
 
 def lengthIsLessThan14(inputString):
 	if len(inputString) < 14:
-		print "\tLength is < 14"
+		if DEBUG:
+			print "\tLength is < 14"
 		return True;
-	print "\tLength is > 14"
+	if DEBUG:
+		print "\tLength is > 14"
 	return False
 
 def numberCount(inputString):
@@ -66,7 +75,8 @@ def numberCount(inputString):
 	for i in inputString:
    		if i.isdigit():
 			digit += 1
-	print "\tnumberCount of " + inputString + " = " + str(digit)
+	if DEBUG:
+		print "\tnumberCount of " + inputString + " = " + str(digit)
 	return digit;
 
 def symbolCount(inputString):
@@ -74,7 +84,8 @@ def symbolCount(inputString):
 	for i in inputString:
 		if i.isalnum() == False:
 			symbolCount += 1
-	print "\tsymbolCount = " + str(symbolCount)
+	if DEBUG:
+		print "\tsymbolCount = " + str(symbolCount)
 	return symbolCount;
 
 def uniqueLetterCount(inputString):
@@ -82,7 +93,8 @@ def uniqueLetterCount(inputString):
         for i in inputString:
 		if i.isalpha() and not i in arr:
 			arr.append(i)
-	print "\tuniqueLetterCount = " + str(len(arr))
+	if DEBUG:
+		print "\tuniqueLetterCount = " + str(len(arr))
 	return len(arr);
 
 def uniqueLowerCaseCount(inputString):
@@ -90,7 +102,8 @@ def uniqueLowerCaseCount(inputString):
 	for i in inputString:
 		if i.isalpha() and not i in arr and i.islower():
 			arr.append(i)
-	print "\tuniqueLowerCaseCount of = " + str(len(arr))
+	if DEBUG:
+		print "\tuniqueLowerCaseCount of = " + str(len(arr))
 	return len(arr);
 
 def uniqueUpperCaseCount(inputString):
@@ -98,5 +111,6 @@ def uniqueUpperCaseCount(inputString):
 	for i in inputString:
 		if i.isalpha() and not i in arr and i.isupper():
 			arr.append(i)
-	print "\tuniqueUpperCaseCount of = " + str(len(arr))
+	if DEBUG:
+		print "\tuniqueUpperCaseCount of = " + str(len(arr))
 	return len(arr);
