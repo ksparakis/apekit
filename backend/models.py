1e001d6d86bce72a14a8e3aae7aaf8650dce81c7
Luke Sorenson <lasoren@me.com>
2015-12-06 20:29:25 -0500

    Moved backend code into module called backend

---
+++ backend/models.py
@@ -0,0 +1,56 @@
+from peewee import *
+
+db = SqliteDatabase('apekit.db')
+
+class App(Model):
+    created = DateField()
+    app_id = CharField(unique=True)
+    title = CharField()
+    version_string = CharField()
+    developer_name = CharField()
+    downloads = BigIntegerField()
+    star_rating = FloatField()
+    metadata_url = TextField()
+    apk_url = TextField()
+    apk_local = TextField(default="")
+
+    class Meta:
+        database = db
+
+
+class Permission(Model):
+    name = TextField()
+    count = IntegerField()
+
+    class Meta:
+        database = db
+
+
+class AppPermission(Model):
+    app = ForeignKeyField(App, related_name='app')
+    permission = ForeignKeyField(Permission,
+        related_name='permission')
+
+    class Meta:
+        database = db
+
+
+class Vulnerability(Model):
+    description = CharField()
+    # How many apps have this vulnerability?
+    count = IntegerField()
+
+    class Meta:
+        database = db
+
+
+class AppVulnerability(Model):
+    app = ForeignKeyField(App, related_name='app_vuln')
+    vulnerability = ForeignKeyField(Vulnerability,
+        related_name='vuln_vuln')
+    filename = TextField()
+    line_number = IntegerField()
+    source_code = TextField()
+
+    class Meta:
+        database = db
