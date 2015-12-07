bf6ce5c70550d4dded287aba02f0141670b8d5f0
ksparakis <ksparakis@carbonite.com>
2015-12-07 15:20:00 -0500

    Refined Function to account for websites and consecutive ascii values. Also added test support by passign in code from failed db stores

--- vulns/keySearch.test.py
+++ vulns/keySearch.test.py
@@ -1,6 +1,14 @@
+import sqlite3
 from keySearch import *
 
-testString = "String hi = \"momomomojndfiausdfaiudhfadifahsdfasuf\""
-print "Initiating a basic test.... Passing in" + testString
-print keySearch(testString)
-print "end of test"
+
+
+print "Beggining test on keySearch..."
+conn = sqlite3.connect('test.db')
+c = conn.cursor()
+for row in c.execute('SELECT source_code FROM appvulnerability WHERE vulnerability_id=10'):
+	result = keySearch(row[0])
+	if result[0]:
+	        print "Passed Key test:\t" + inQuotes(row[0])[0]
+		#print "line of code:\t" + row[0]
+print "End of test on keySearch..."
