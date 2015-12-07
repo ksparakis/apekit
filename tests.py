426aeeffe2daaa53d7a5d63f9df99e4b9da12aa0
Carlton Duffett <cjduffett@comcast.net>
2015-12-06 21:17:10 -0500

    added crawler tests

--- tests.py
+++ tests.py
@@ -1,3 +1,10 @@
 from tests.model_test import *
+from tests.crawler_test import *
 
+# model tests
 add_apps_to_db_test()
+
+# crawler tests
+crawler_sample_archive_test()
+crawler_get_permissions_test()
+crawler_download_test()
--- tests/crawler_test.py
+++ tests/crawler_test.py
@@ -1,6 +1,41 @@
-from orangutan.crawler import ArchiveCrawler
+import os
+import shutil
+from crawler.crawler import ArchiveCrawler
 
 c = ArchiveCrawler()
-c.sample(3)
-c.get_permissions()
-c.download()
+
+def crawler_sample_archive_test():
+    try:
+        c.sample(3)
+        print "crawler_sample_archive_test: PASS"
+    except * as err:
+        print err
+        print "crawler_sample_archive_test: FAIL"
+    
+def crawler_get_permissions_test():
+    try:
+        c.get_permissions()
+        print "crawler_get_permissions_test: PASS"
+    
+    except * as err:
+        print err
+        print "crawler_get_permissions_test: FAIL"
+    
+def crawler_download_test():
+    try:
+        c.download()
+        print "crawler_download_test: PASS"
+        
+    except * as err:
+        print err
+        print "crawler_download_test: FAIL"
+        
+def crawler_test_cleanup():
+    
+    # delete database
+    os.remove("./*.db")
+    
+    # delete downloaded apks
+    shutil.rmtree("./apks/")
+    
+    print "crawler_test_cleanup: DONE"
